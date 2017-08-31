a2ace2d3772aed97a7fd53a53b81108c2d1c89f5
ryanbrown358 <ryanbrown358@gmail.com>
2017-08-31 11:12:59 -0700

    Add files via upload

---
+++ to-do/app.js
@@ -0,0 +1,17 @@
+angular.module('ToDo', []).
+controller('todoController',['$scope',function($scope){
+  $scope.todos = [
+    {'title': 'Build a todo app','done':false}
+  ];
+    $scope.addTodo = function(){
+      $scope.todos.push({'title': $scope.newTodo, 'done':false})
+      $scope.newTodo = ''
+    }
+    $scope.clearCompleted = function(){
+      $scope.todos = $scope.todos.filter(function(item){
+        return !item.done
+      })
+    }
+
+
+}])
